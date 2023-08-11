1555ec1e3284f746c262b7629bba64ce1057330f
Akshay Pratap Singh <99736940+AkshayPratapSingh09@users.noreply.github.com>
2023-08-11 23:56:25 +0530

    Update AppContext.js

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,7 +1,6 @@
 import React, { createContext, useReducer } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
-// 5. The reduceer - this is used to update the state, based on the action
 export const AppReducer = (state, action) => {
 	switch (action.type) {
     case "ADD_EXPENSE":
